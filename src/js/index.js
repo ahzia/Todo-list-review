20f7b56a8e18338c749cf213821dfa9e87f1dcf2
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-08-31 13:18:05 +0430

    all codes added from main repo

---
+++ src/js/index.js
@@ -0,0 +1,104 @@
+import '../css/style.css';
+import List from './list.js';
+import getDragAfterElement from './drag.js';
+
+const newList = new List();
+const eventListener = () => {
+  document.querySelector('ul').onclick = (event) => {
+    const { target } = event;
+    const { id } = target;
+    if (id.includes('idel')) {
+      const divId = (id).replace('idel', '');
+      newList.remove(divId);
+      eventListener();
+    } else if (id.includes('del')) {
+      const divId = (id).replace('del', '');
+      newList.remove(divId);
+      eventListener();
+    } else if (id.includes('ch')) {
+      const index = id.replace('ch', '');
+      newList.updateStatus(index);
+    } else if (id.includes('in')) {
+      const input = document.getElementById(id);
+      const index = id.replace('in', '');
+      const liId = `li${index}`;
+      const inputContainer = document.getElementById(liId);
+      const moveDivId = `mv${index}`;
+      const moveDiv = document.getElementById(moveDivId);
+      const deleteDivId = `del${index}`;
+      const deleteDiv = document.getElementById(deleteDivId);
+      const liFocus = document.querySelector('.liFocus');
+      const hidden = document.querySelector('.hidetemp');
+      const visible = document.querySelector('.visible');
+      const inFocus = document.querySelector('.inFocus');
+      if (visible) {
+        visible.classList.remove('visible');
+      }
+      if (hidden) {
+        hidden.classList.remove('hidetemp');
+      }
+      if (liFocus) {
+        liFocus.classList.remove('liFocus');
+      }
+      if (inFocus) {
+        inFocus.classList.remove('inFocus');
+      }
+      inputContainer.classList.add('liFocus');
+      moveDiv.classList.add('hidetemp');
+      deleteDiv.classList.add('visible');
+      input.classList.add('inFocus');
+      input.addEventListener('change', (e) => {
+        const { value } = e.target;
+        newList.update(index, value);
+      });
+    }
+  };
+  const draggables = document.querySelectorAll('.draggable');
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+    });
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging');
+    });
+  });
+  const container = document.getElementById('list');
+  container.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector('.dragging');
+    if (afterElement == null) {
+      // end of list
+      container.appendChild(draggable);
+    } else {
+      // insert before (after element)
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+  container.addEventListener('drop', (event) => {
+    event.preventDefault();
+    newList.arrangeTodos();
+    newList.updateLocalStorage(true, true);
+    // reload the page to refresh the event EventListener
+    eventListener();
+  }, false);
+};
+// This first function ensures that the document has being already created
+document.addEventListener('DOMContentLoaded', () => {
+  eventListener();
+  const addButton = document.getElementById('add');
+  addButton.addEventListener('click', () => {
+    const input = document.getElementById('todoInput');
+    const text = input.value;
+    if (text !== '') {
+      newList.addNew(text);
+      input.value = '';
+      eventListener();
+    }
+  });
+  const clearAllButton = document.getElementById('clearAll');
+  clearAllButton.addEventListener('click', () => {
+    newList.clearAll();
+    eventListener();
+  });
+});
---
+++ src/js/list.js
@@ -0,0 +1,140 @@
+export default class todoList {
+  constructor() {
+    this.todos = [];
+    this.updateLocalStorage();
+  }
+
+addNew = (value) => {
+  const index = this.todos.length;
+  const newtodo = {
+    index,
+    description: value,
+    completed: false,
+  };
+  this.todos.push(newtodo);
+  this.updateLocalStorage(true, true);
+}
+
+remove = (index) => {
+  this.todos.splice(index, 1);
+  this.updateIndex();
+  this.updateLocalStorage(true, false);
+}
+
+update = (index, value) => {
+  this.todos[index].description = value;
+  this.updateLocalStorage(true, false, false);
+}
+
+clearAll = () => {
+  this.todos = this.todos.filter((todo) => !todo.completed);
+  this.updateIndex();
+  this.updateLocalStorage(true, false, true);
+}
+
+updateIndex = () => {
+  const temp = [];
+  let counter = 0;
+  this.todos.forEach((item) => {
+    const todo = {
+      index: counter,
+      description: item.description,
+      completed: item.completed,
+    };
+    temp.push(todo);
+    counter += 1;
+  });
+  this.todos = temp;
+}
+
+sortList = () => {
+  this.todos.sort((todoA, todoB) => {
+    if (todoA.index < todoB.index) {
+      return -1;
+    }
+    if (todoA.index > todoB.index) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+displayList = () => {
+  const section = document.getElementById('todos');
+  const list = document.createElement('ul');
+  list.id = 'list';
+  this.todos.forEach((todo) => {
+    const { description, completed, index } = todo;
+    const liId = `li${index}`;
+    const checkboxId = `ch${index}`;
+    const inputId = `in${index}`;
+    const moveDiv = `mv${index}`;
+    const deleteDiv = `del${index}`;
+    let todoCard = `<li id=${liId} class="todo draggable" draggable="true"><div class="todoContent">`;
+    if (completed) {
+      todoCard += `<input type="checkbox" name="todoCheck" class="todoCheck" checked id="${checkboxId}">`;
+    } else {
+      todoCard += `<input type="checkbox" name="todoCheck" class="todoCheck" id="${checkboxId}">`;
+    }
+    todoCard += `
+      <input type="text" class="description" id="${inputId}" value="${description}">
+      </div>
+      <div id="${moveDiv}" class="draggableAria">
+        <i class="fas fa-ellipsis-v"></i>
+      <div>
+      <div id="${deleteDiv}" class="deleteDiv hidden">
+        <i id="i${deleteDiv}" class="fas fa-trash-alt"></i>
+      <div>
+      </li>`;
+    list.insertAdjacentHTML('beforeend', todoCard);
+  });
+  section.innerHTML = '';
+  section.appendChild(list);
+};
+
+updateLocalStorage = (update, sort, display = true) => {
+  const listFromStorage = window.localStorage.getItem('todos');
+  if (update) {
+    // update local storage
+    if (sort) {
+      this.sortList();
+    }
+    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+  } else if (listFromStorage != null) {
+    // get data from local storage
+    this.todos = JSON.parse(window.localStorage.getItem('todos'));
+  } else {
+    // inialize local storage
+    this.sortList();
+    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  if (display) {
+    this.displayList();
+  }
+};
+
+updateStatus = (index) => {
+  this.todos[index].completed = !(this.todos[index].completed);
+  this.updateLocalStorage(true, false, false);
+};
+
+arrangeTodos = () => {
+  const container = document.getElementById('list');
+  const children = container.childNodes;
+  const childarray = [...children];
+  let index = 0;
+  const temp = [];
+  childarray.forEach((item) => {
+    const { id } = item;
+    const newIndex = ((id).replace('li', '')).replace('li', '');
+    const todo = {
+      index,
+      description: this.todos[newIndex].description,
+      completed: this.todos[newIndex].completed,
+    };
+    temp.push(todo);
+    index += 1;
+  });
+  this.todos = temp;
+};
+}
